c1a7afd7f5b6a536b6dfccdbdf39c03b56165c0e
oqtoshqoratoshgapirmaganqozichoq <epaxaboz@britishpreschool.net>
2022-08-06 11:08:53 +0500

    components crearted

---
+++ src/components/Card.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Card() {
+    return (
+        <div>
+            <div className="card">
+                <img src="https://picsum.photos/400/500" alt="" />
+                <h4>Title nimadir</h4>
+            </div>
+        </div>
+    )
+}
