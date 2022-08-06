75402d05473d212f3e9131d791eb3a04f75cfaf3
oqtoshqoratoshgapirmaganqozichoq <epaxaboz@britishpreschool.net>
2022-08-06 10:30:46 +0500

    some change in files

---
+++ src/konspekt.js
@@ -0,0 +1,14 @@
+// 1. git init = > git initialsize -> ishga tushirish
+// 2. git clone link_to_repsoitory -> repositoryni download qilib oladi
+// 3. git install => package.jsondagi barcha packagelarni install qilib beradi.
+
+// 4. git status => nimalar o'zgarganini ko'tsatib beradi.
+// 5. git add file_nomi|papka_nomi|. => gitga ...ni qo'shish
+// 6. git commit -m "koment qoldirish" => o'zgarishni nomlan olish. kim tomonidan qoldirilgan bu komment. aniqlash mumkin.
+// 7. git push orign main => main degan branchga o'zgarishlarni qo'shish
+// 7. git push => asosiy branchga o'zgarishlarni qo'shish
+// 8. git pull => oxirgi o'xgarishlarni qabul qilib oladi.
+
+
+
+
