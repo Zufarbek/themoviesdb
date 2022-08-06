0bcd782a8a61ba043692edb1379c4bc9f38cd06d
Nurbek2001111 <jurayevnurbek112@gmail.com>
2022-08-06 10:54:32 +0500

    app.js changend first time

--- src/App.js
+++ src/App.js
@@ -4,6 +4,14 @@ export default function App() {
   return (
     <div>
       HOME
+      <p>
+        <div>
+          <img src='' alt=""/>
+          <p>
+            lorem dasloa sdfkj ;lkjf;lakdsj ;lkjasd;fl kajsd;lfkajs;dkl jfd
+          </p>
+        </div>
+      </p>
     </div>
   )
 }
