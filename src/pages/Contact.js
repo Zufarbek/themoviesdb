97ceb281300882c93cf2a8894d62d7d433c9419d
oqtoshqoratoshgapirmaganqozichoq <epaxaboz@britishpreschool.net>
2022-08-06 11:05:52 +0500

    pages created

---
+++ src/pages/Contact.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+export default function Contact() {
+    return (
+        <div>
+            Contact sahifasi
+            <div>
+                <img src="https://piscum.photos/300/400" alt="" />
+                <img src="https://piscum.photos/300/400" alt="" />
+                <img src="https://piscum.photos/300/400" alt="" />
+                <img src="https://piscum.photos/300/400" alt="" />
+            </div>
+            <div>
+                <p>
+                    lortemasd fj;alksdjf;lkasjd ;lfkjas;dk fjad;lsk j;kfsdj;dl
+                </p>
+            </div>
+            <div>
+                <p>
+                    lortemasd fj;alksdjf;lkasjd ;lfkjas;dk fjad;lsk j;kfsdj;dl
+                </p>
+            </div>
+            <div>
+                <img src="https://piscum.photos/300/400" alt="" />
+                <img src="https://piscum.photos/300/400" alt="" />
+                <img src="https://piscum.photos/300/400" alt="" />
+                <img src="https://piscum.photos/300/400" alt="" />
+            </div>
+        </div>
+    )
+}
